a7bb1712dff19538bb15cad6e7a7fd8a2a315a93
Usman King <usmanking6516@gmail.com>
2026-09-09 15:22:59 +0500

    ready for vercel deployment

--- config/admin.ts
+++ config/admin.ts
@@ -1,5 +1,5 @@
-// Yahan aap apna admin password apni marzi ka set kar sakte hain
 export const ADMIN_CONFIG = {
   adminEmail: "usmanbrand5911@gmail.com",
-  adminPassword: "YourSecretAdminPassword123", // Yahan apna password change kar lein
+  adminPassword: process.env.NEXT_PUBLIC_ADMIN_PASSCODE || "admin123",
 };
+;
