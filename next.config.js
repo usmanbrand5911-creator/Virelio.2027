a33685819cc502342cdaad18449847788106046f
usmanbrand5911-creater <usmanbrand5911@gmail.com>
2026-09-10 17:15:18 +0500

    Clean next.config.js and use webpack build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  images: { unoptimized: true },
-};
+  reactStrictMode: true,
+}
+
 module.exports = nextConfig;
